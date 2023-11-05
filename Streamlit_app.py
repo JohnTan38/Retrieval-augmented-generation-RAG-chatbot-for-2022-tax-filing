6ab43ba11db3b8925867acdb8f8d30c4e5983a06
Fei Han <59255164+hanfei1986@users.noreply.github.com>
2023-11-05 12:11:54 -0500

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -67,9 +67,10 @@ if st.session_state.messages[-1]["role"] != "assistant":
             prompt_retrived_content = vectorstore.similarity_search(st.session_state.messages[-1]["content"], k=5)
             # Concatenate the retrieved prompts
             new_prompt = ""
-            for document in prompt_retrived_content:
+            #for document in prompt_retrived_content:
                 #new_prompt += str(document).split("page_content='")[1].split("', metadata=")[0].replace('\n','')+". "
-                new_prompt += document.page_content
+            for i in range(5):
+                new_prompt += prompt_retrived_content[i].page_content
             new_prompt += st.session_state.messages[-1]["content"]
             # Replace the original prompt with the concatenated prompts
             new_messages = st.session_state.messages.copy()
